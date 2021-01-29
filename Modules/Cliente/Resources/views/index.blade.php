a340b8edba96e4d88c84561189f103f8d37977e9
Eduardo Aguilar <ozieldevelop@gmail.com>
2021-01-29 21:30:22 +0000

    Nuevo modulo de clientes

---
+++ Modules/Cliente/Resources/views/index.blade.php
@@ -0,0 +1,176 @@
+@extends('layouts.cliente')
+
+@section('content')
+
+
+  <style>
+  
+
+    
+    .checkbox label:after, 
+.radio label:after {
+    content: '';
+    display: table;
+    clear: both;
+}
+
+.checkbox .cr,
+.radio .cr {
+    position: relative;
+    display: inline-block;
+    border: 1px solid #a9a9a9;
+    border-radius: .25em;
+    width: 1.3em;
+    height: 1.3em;
+    float: left;
+    margin-right: .5em;
+}
+
+.radio .cr {
+    border-radius: 50%;
+}
+
+.checkbox .cr .cr-icon,
+.radio .cr .cr-icon {
+    position: absolute;
+    font-size: .8em;
+    line-height: 0;
+    top: 50%;
+    left: 20%;
+}
+
+.radio .cr .cr-icon {
+    margin-left: 0.04em;
+}
+
+.checkbox label input[type="checkbox"],
+.radio label input[type="radio"] {
+    display: none;
+}
+
+.checkbox label input[type="checkbox"] + .cr > .cr-icon,
+.radio label input[type="radio"] + .cr > .cr-icon {
+
+    opacity: 0;
+
+}
+
+.checkbox label input[type="checkbox"]:checked + .cr > .cr-icon,
+.radio label input[type="radio"]:checked + .cr > .cr-icon {
+    transform: scale(1) rotateZ(0deg);
+    opacity: 1;
+}
+
+.checkbox label input[type="checkbox"]:disabled + .cr,
+.radio label input[type="radio"]:disabled + .cr {
+    opacity: .5;
+}
+    
+    
+    
+  </style>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+
+
+
+    <!-- Heading Row -->
+    <div class="row align-items-center my-5">
+      <div class="col-lg-2">
+        <img class="img-fluid rounded mb-7 mb-lg-0" src="http://placehold.it/900x400" alt="">
+      </div>
+      <!-- /.col-lg-8 -->
+      <div class="col-lg-10">
+        <h1 class="font-weight-light">{{  env('TITULOREGISTROASPIRANTES') }}</h1>
+        <!--p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it makes a great use of the standard Bootstrap core components. Feel free to use this template for any project you want!</p-->
+        <a class="btn btn-primary" href="#">Subir nueva imagen de perfil!</a>
+      </div>
+      <!-- /.col-md-4 -->
+    </div>
+    <!-- /.row -->
+
+    <!-- Call to Action Well -->
+    <div class="card text-white bg-secondary my-5 py-4 text-center">
+      <div class="card-body">
+        <p class="text-white m-0">Seleccione las &aacute;reas a las que desea postularse. (este listado ser&aacute; verificado)</p>
+      </div>
+    </div>
+
+
+
+
+
+<form>
+  
+    <div class="form-group row">
+      <label for="staticEmail" class="col-sm-2 col-form-label">N&uacute;mero de cliente</label>
+      <div class="col-sm-10">
+        <input type="text" readonly class="form-control-plaintext" id="numero_cliente" value="AB123">
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="staticEmail" class="col-sm-2 col-form-label">Nombre de cliente</label>
+      <div class="col-sm-10">
+        <input type="text" readonly class="form-control-plaintext" id="nombre" value="JORGE RODRIGUEZ">
+      </div>
+    </div>  
+
+  
+      <p style="color: #c2c9d0;font-weight: bold;font-size: 18px;">
+        Seleccione las &aacute;reas a las que desea postularse. (este listado ser&aacute; verificado)
+    </p>
+
+  
+
+    <div class="col-sm-12 col-md-4 col-lg-4">
+        <div class="checkbox">
+            <label style="font-size:27px">
+                <input type="checkbox" value="" checked>
+                <span class="cr"><i class="cr-icon fa fa-check"></i></span>
+                Junta de Directores
+            </label>
+        </div>
+    </div>
+  
+    <div class="col-sm-12 col-md-4 col-lg-4">
+        <div class="checkbox">
+            <label style="font-size: 27px">
+                <input type="checkbox" value="" checked>
+                <span class="cr"><i class="cr-icon fa fa-check"></i></span>
+                Junta de Seguridad
+            </label>
+        </div>
+    </div>
+   
+  
+    <div class="col-sm-12 col-md-4 col-lg-4">
+        <div class="checkbox">
+            <label style="font-size:27px">
+                <input type="checkbox" value="" checked>
+                <span class="cr"><i class="cr-icon fa fa-check"></i></span>
+                Comit&eacute; de Cr&eacute;dito
+            </label>
+        </div>
+      <br/><br/>
+    </div>
+   
+  
+  
+  
+    <div class="form-group ">
+      <label for="exampleFormControlFile1">Hoja de vida</label>
+      <input type="file" class="form-control-file" id="exampleFormControlFile1">
+    </div>
+
+    <div class="form-group row">
+       <div class="col-sm-10"><br/><br/><br/><br/><br/>
+          <button type="submit" class="btn btn-primary">Registrar</button>
+        </div>
+    </div>
+  
+  
+</form>
+
+
+
+@endsection
---
+++ resources/views/layouts/cliente.blade.php
@@ -0,0 +1,191 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <title>{{  env('COOPENOMBRE') }} </title>
+    <meta content="Responsive admin theme build on top of Bootstrap 4" name="description" />
+    <meta content="CooperativaRLdesign" name="author" />
+	  <meta name="csrf-token" content="{{ csrf_token() }}">	
+		
+    <link rel="shortcut icon" href="../../assets/images/favicon.ico">
+
+    <link href="../../assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="../../assets/css/metismenu.min.css" rel="stylesheet" type="text/css">
+    <link href="../../assets/css/icons.css" rel="stylesheet" type="text/css">
+    <link href="../../assets/css/style.css" rel="stylesheet" type="text/css">
+
+  
+      <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+
+  
+      <!--link href="https://cdn.datatables.net/responsive/2.2.6/css/responsive.dataTables.min.css" rel="stylesheet" type="text/css">
+      <script src="https://cdn.datatables.net/responsive/2.2.6/js/dataTables.responsive.min.js"></script-->
+  
+
+	
+	
+	
+    <script src="{{ asset('js/sweetalert2.min.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('css/sweetalert2.min.css') }}">
+    <script src="{{ asset('js/herramientas.js') }}"></script>
+	
+	
+    <script>
+
+        $.ajaxSetup({
+            headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+          });
+  
+
+
+		function nobackbutton(){
+
+
+		   window.location.hash="no-back-button";
+
+
+		   window.location.hash="Again-No-back-button" //chrome
+
+
+		   window.onhashchange=function(){window.location.hash="no-back-button";}
+			
+
+		}
+
+
+    </script>
+	
+</head>
+
+<body >
+
+    <div class="header-bg">
+        <!-- Navigation Bar-->
+        <header id="topnav">
+            <div class="topbar-main">
+                <div class="container-fluid">
+
+                    <!-- Logo-->
+                    <div>
+                        <a href="#" class="logo" style="">
+                            <span class="logo-light">
+                                   {{  env('COOPENOMBRE') }}
+								
+                            </span> <br/> 
+							
+                        </a>
+                    </div>
+                    <!-- End Logo-->
+
+                    <div class="menu-extras topbar-custom navbar p-0">
+                        <!--ul class="list-inline d-none d-lg-block mb-0">
+                            <li class="hide-phone app-search float-left">
+                                <form role="search" class="app-search">
+                                    <div class="form-group mb-0">
+                                        <input type="text" class="form-control" placeholder="Search..">
+                                        <button type="submit"><i class="fa fa-search"></i></button>
+                                    </div>
+                                </form>
+                            </li>
+                        </ul-->
+
+                        <ul class="navbar-right ml-auto list-inline float-right mb-0">
+                            <!-- language-->
+                            <!--li class="dropdown notification-list list-inline-item d-none d-md-inline-block">
+                                <a class="nav-link dropdown-toggle arrow-none waves-effect" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                                    <img src="../../assets/images/flags/us_flag.jpg" class="mr-2" height="12" alt="" /> English <span class="mdi mdi-chevron-down"></span>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right dropdown-menu-animated language-switch">
+                                    <a class="dropdown-item" href="#"><img src="../../assets/images/flags/french_flag.jpg" alt="" height="16" /><span> French </span></a>
+                                    <a class="dropdown-item" href="#"><img src="../../assets/images/flags/spain_flag.jpg" alt="" height="16" /><span> Spanish </span></a>
+                                    <a class="dropdown-item" href="#"><img src="../../assets/images/flags/russia_flag.jpg" alt="" height="16" /><span> Russian </span></a>
+                                    <a class="dropdown-item" href="#"><img src="../../assets/images/flags/germany_flag.jpg" alt="" height="16" /><span> German </span></a>
+                                    <a class="dropdown-item" href="#"><img src="../../assets/images/flags/italy_flag.jpg" alt="" height="16" /><span> Italian </span></a>
+                                </div>
+                            </li-->
+
+                            <!-- full screen -->
+                            <!--li class="dropdown notification-list list-inline-item d-none d-md-inline-block">
+                                <a class="nav-link waves-effect" href="#" id="btn-fullscreen">
+                                    <i class="mdi mdi-arrow-expand-all noti-icon"></i>
+                                </a>
+                            </li-->
+
+                          
+
+                        </ul>
+
+                    </div>
+                    <!-- end menu-extras -->
+
+                    <div class="clearfix"></div>
+
+                </div>
+                <!-- end container -->
+            </div>
+            <!-- end topbar-main -->
+
+            <!-- MENU Start -->
+            <div class="navbar-custom">
+                <div class="container-fluid">
+
+                    <div id="navigation">
+
+                        <ul class="navigation-menu">
+
+       
+
+                        </ul>
+                        <!-- End navigation menu -->
+                    </div>
+                    <!-- end #navigation -->
+                </div>
+                <!-- end container -->
+            </div>
+            <!-- end navbar-custom -->
+        </header>
+        <!-- End Navigation Bar-->
+
+    </div>
+    <!-- header-bg -->
+
+    <div class="wrapper" style="padding-top: 84px;">
+        <div class="container-fluid">
+ 
+ 
+          
+          @yield('content')
+          
+        </div>
+        <!-- end container-fluid -->
+    </div>
+    <!-- end wrapper -->
+
+    <!-- Footer -->
+    <footer class="footer">
+        ©  {{ date("Y")  }}  </title> <span class="d-none d-sm-inline-block"> - Cooperativa Profesionales, R.L.</span>.
+    </footer>
+
+    <!-- End Footer -->
+
+    <!-- jQuery  -->
+    <script src="../../assets/js/jquery.min.js"></script>
+    <script src="../../assets/js/bootstrap.bundle.min.js"></script>
+    <script src="../../assets/js/jquery.slimscroll.js"></script>
+    <script src="../../assets/js/waves.min.js"></script>
+
+    <!-- App js -->
+    <script src="../../assets/js/app.js"></script>
+
+</body>
+
+   @yield('page-script')
+  
+  
+</html>
