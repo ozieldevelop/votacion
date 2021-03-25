e17afbe05c166d708af003d680eca8efee6f064a
Eduardo Aguilar <ozieldevelop@gmail.com>
2021-03-25 00:55:18 -0500

    correción en etiqueta de listado de propuestas

--- Modules/Cliente/Resources/views/tablero.blade.php
+++ Modules/Cliente/Resources/views/tablero.blade.php
@@ -9,11 +9,11 @@
 <div class="col-sm-12 col-md-12 col-lg-12"> &nbsp;</div>
 
 
-<div class="col-sm-12 col-md-12 col-lg-12">     
+<div class="col-sm-12 col-md-12 col-lg-12">
     <div class="card" >
         <div class="card-header bg-light resaltado">ADJUNTOS  </div>
         <div class="card-body" >
-            
+
             <!-- AREA DONDE SE LISTARAN LOS ARCHIVOS ADJUNTOS UNA VEZ SUBIDOS -->
             <table  class="table" style="width:100%" id="gs_tbl_GestionesArchivos"> </table>
         </div>
@@ -24,13 +24,13 @@
 
 @if($tipoevent==2)
 
-<div class="col-sm-12 col-md-12 col-lg-12" style="display:none">     
-    
-    
+<div class="col-sm-12 col-md-12 col-lg-12" style="display:none">
+
+
     <div class="card" >
         <div class="card-header bg-light resaltado"> <label style="float-right; color:#c36c55"> NUEVA </label> PROPUESTAS</div>
         <div class="card-body" >
-            
+
             <table class="table" style="width:100%">
                 <thead>
                     <tr>
@@ -40,27 +40,27 @@
                     </tr>
                 </thead>
                 <tbody id="PropuestasDir">
-                    
+
                 </tbody>
             </table>
-            
+
         </div>
     </div>
-    
-</div> 
+
+</div>
 
 @endif
 
 
-<div class="col-sm-12 col-md-12 col-lg-12"> &nbsp;</div>    
+<div class="col-sm-12 col-md-12 col-lg-12"> &nbsp;</div>
 
 
 @if($tipoevent==2)
-<div class="col-sm-12 col-md-12 col-lg-12"> 
+<div class="col-sm-12 col-md-12 col-lg-12">
     {{-- <div class="card" >
         <div class="card-header bg-light resaltado">POSTULADOS</div>
         <div class="card-body" >
-            
+
             <table class="table" style="width:100%">
                 <thead>
                     <tr>
@@ -70,10 +70,10 @@
                     </tr>
                 </thead>
                 <tbody id="DirectivosDir">
-                    
+
                 </tbody>
             </table>
-            
+
         </div>
     </div> --}}
     <div class="card border-success">
@@ -95,7 +95,7 @@
                             <div class="form-row">
                                 <div class="col-md-12 col-sm-12">
                                     <label for="titulo">Título:</label>
-                                    <input type="text" name="titulo" class="form-control" placeholder="Título de su propuesta" 
+                                    <input type="text" name="titulo" class="form-control" placeholder="Título de su propuesta"
                                         v-model="propuesta.titulo" required>
                                 </div>
                             </div>
@@ -103,7 +103,7 @@
                             <div class="form-row mb-2">
                                 <div class="col-md-12 col-sm-12">
                                     <label for="detalle">Detalles adicionales de la propuesta:</label>
-                                    <textarea type="text" name="detalle" class="form-control" placeholder="Mayores detalles." 
+                                    <textarea type="text" name="detalle" class="form-control" placeholder="Mayores detalles."
                                         v-model="propuesta.detalle" rows="7" cols="10">
                                     </textarea>
                                 </div>
@@ -111,12 +111,12 @@
                             <div class="form-row mb-2">
                                 <div class="col-md-6 col-sm-12">
                                     <label for="secunda_asoc">Secunda la propuesta:</label>
-                                    <input type="text" name="secunda_asoc" class="form-control" placeholder="Secundada Por:" 
+                                    <input type="text" name="secunda_asoc" class="form-control" placeholder="Secundada Por:"
                                         v-model="propuesta.secunda_asoc" required>
                                 </div>
                                 <div class="col-md-6 col-sm-12">
                                     <label for="secunda_asoc_id">Número de Cliente:</label>
-                                    <input type="number" name="secunda_asoc_id" class="form-control" placeholder="# de cliente:" 
+                                    <input type="number" name="secunda_asoc_id" class="form-control" placeholder="# de cliente:"
                                         v-model="propuesta.secunda_asoc_id" required>
                                 </div>
                             </div>
@@ -130,7 +130,7 @@
                     </div>
                     <br><hr>
                     <div class="col-md-12">
-                        <h3>Listado de mis Propuestas</h3>
+                        <h3>Listado de Propuestas</h3>
                         <br>
                         <template>
                         <ul class="list-group">
@@ -162,24 +162,24 @@
                     </div>
                 {{-- </template> --}}
             </div>
-            
+
             <button class="btn btn-success" onclick="bloquearpantallasala(1)"  style="display:none"> BOT&Oacute;N ENVIAR ACCION A TODOS LOS PERTENECIENTES A SALA CAPITULAR </button>
             <br/>
             <br/>
             <ul class="" role="menu" data-accordion="false" id="usuarioslinea">
             </ul>
-            
+
         </div>
     </div>
-</div>         
-@endif  
+</div>
+@endif
 
 
 
 
-<div class="col-sm-12 col-md-12 col-lg-12" style="display:none">        
-    
-    
+<div class="col-sm-12 col-md-12 col-lg-12" style="display:none">
+
+
     <div class="card card-secondary card-tabs">
         <div class="card-header p-0 pt-1">
             <ul class="nav nav-tabs" id="custom-tabs-one-tab" role="tablist">
@@ -194,26 +194,26 @@
         <div class="card-body">
             <div class="tab-content" id="custom-tabs-one-tabContent">
                 <div class="tab-pane fade show active" id="custom-tabs-one-home" role="tabpanel" aria-labelledby="custom-tabs-one-home-tab">
-                    
+
                     <div class="row">
                         <div class="col-12">
-                            
-                            
+
+
                             <div class="card-body">
-                                
+
                             </div>
                             <!-- /.card-body -->
-                            
-                            
+
+
                         </div>
-                        
-                    </div>       
+
+                    </div>
                 </div>
-                
+
             </div>
         </div>
     </div>
-</div> 
+</div>
 
 
 
@@ -228,12 +228,12 @@
         <!-- /.card-header -->
         <div class="card-body p-0">
             <textarea id="codeMirrorDemo" class="p-3">
-                
-                
+
+
             </textarea>
         </div>
         <div class="card-footer">
-            
+
         </div>
     </div>
     <br />
@@ -248,17 +248,17 @@
         background: #c3c3c3;
         width: 5%;
     }
-    
+
     .card {
-        
+
         margin-bottom: 0px;
     }
-    
+
     .img-fluid {
         max-width: 56%;
-        
+
     }
-    
+
 </style>
 
 
@@ -274,7 +274,7 @@
 
 
 <script>
-    
+
     var modelo = {
         'id_evento':'{{ $id_evento }}',
         'nombre_evento':'{{ $nombreevento }}',
@@ -290,7 +290,7 @@
         'f_asistire_regis':'',
         'soy_aspirante':''
     };
-    
+
     /*
     Dropzone.options.myDropzone = {
         paramName: 'file',
@@ -306,18 +306,18 @@
         }
     };
     */
-    
-    
+
+
     function haybloqueo()
     {
         var parametros = {
-            sala : {{ $id_evento }} 
+            sala : {{ $id_evento }}
         }
-        socket.emit('atencion', parametros, function(err) { });      
+        socket.emit('atencion', parametros, function(err) { });
     }
-    
-    
-    
+
+
+
     /*
     $(function () {
         $(document).on('click', '[data-toggle="lightbox"]', function(event) {
@@ -326,7 +326,7 @@
                 alwaysShowClose: true
             });
         });
-        
+
         //$('.filter-container').filterizr({gutterPixels: 3});
         $('.btn[data-filter]').on('click', function() {
             $('.btn[data-filter]').removeClass('active');
@@ -334,23 +334,23 @@
         });
     });
     */
-    
+
     CodeMirror.fromTextArea(document.getElementById("codeMirrorDemo"), {
         mode: "htmlmixed",
         theme: "monokai"
-    });    
-    
+    });
+
     cargarperfil();
-    
+
     function cargarperfil()
     {
-        
+
         document.querySelector('.CodeMirror').CodeMirror.setValue(JSON.stringify(modelo,undefined,2))
-        
-    }  
-    
-    
-    
+
+    }
+
+
+
 </script>
 
 
@@ -377,71 +377,71 @@
         }
     }
     */
-    
-    function buildHtmlTableAdjuntos(adjuntosed,selector) 
+
+    function buildHtmlTableAdjuntos(adjuntosed,selector)
     {
         var columns = addAllColumnHeadersAdjuntos(adjuntosed,selector);
         var contadoradjuntos = 0;
         var bandera = -1;
-        
-        
-        for (var i = 0; i < adjuntosed.length; i++) 
+
+
+        for (var i = 0; i < adjuntosed.length; i++)
         {
             if(adjuntosed[i] != undefined)
             {
-                
+
                 var row$ = $('<tr id=rowNum'+contadoradjuntos+' />');
-                for (var colIndex = 0; colIndex < columns.length; colIndex++) 
+                for (var colIndex = 0; colIndex < columns.length; colIndex++)
                 {
                     //console.log(adjuntosed);
                     bandera++;
                     var cellValue = adjuntosed[i][columns[colIndex]];
                     //console.log(adjuntosed[i]);
                     if (cellValue == null) cellValue = "";
-                    
+
                     if(bandera<=1)
                     {
                         row$.append($('<td style="word-wrap: break-word;" />').html(cellValue));
-                        
+
                     }
                     else
                     {
-                        var eldiv = document.createElement("div");	
+                        var eldiv = document.createElement("div");
                         eldiv.setAttribute("role", "group");
                         eldiv.setAttribute("class", "btn-group-vertical");
-                        
-                        
-                        
+
+
+
                         var elboton2 = document.createElement("button");
                         elboton2.setAttribute("type", "button");
                         elboton2.setAttribute("onclick", "javascript:descargaradjunto("+cellValue+")");
                         elboton2.setAttribute("class", "btn btn-info col-12");
                         elboton2.innerHTML ="Descargar";
-                        
+
                         var eltdfinal = document.createElement("td");
                         eltdfinal.setAttribute("style","word-wrap: break-word");
                         eltdfinal.appendChild(eldiv);
                         eltdfinal.appendChild(elboton2);
-                        row$.append(eltdfinal);	
-                        
+                        row$.append(eltdfinal);
+
                     }
                 }
-                
+
                 if(bandera===2)
                 {
                     bandera=-1;
                 }
-                
+
             }
             contadoradjuntos++;
             $(selector).append(row$);
         }
-        
-        
+
+
     }
-    
-    
-    
+
+
+
     function addAllColumnHeadersAdjuntos(elementosDatax1, selector)
     {
         var columnSet = [];
@@ -465,23 +465,23 @@
             $(selector).html('<thead class="thead-light"><tr><th>No se encontraron registros</th></tr></thead>');
             return columnSet;
         }
-        
+
         $(selector).html(elthead);
         return columnSet;
     }
-    
-    
+
+
     function cargaadjuntosx(opcion)
     {
         // console.log("aaa");
         var id_evento = $('#eventos').val();
         $.ajax
         ({
-            url: '{{ url("cliente/cargaadjuntosScreenListar")}}/'  
-            , data: { 'id_evento': {{ $id_evento }}  }             
+            url: '{{ url("cliente/cargaadjuntosScreenListar")}}/'
+            , data: { 'id_evento': {{ $id_evento }}  }
             , method: 'get'
             ,success: function(datos){
-                //adjuntosed = datos; 
+                //adjuntosed = datos;
                 $('#gs_tbl_GestionesArchivos').html('');
                 buildHtmlTableAdjuntos(datos,'#gs_tbl_GestionesArchivos');
             },
@@ -491,21 +491,21 @@
             }
         });
     }
-    
+
     function evaluarasignararea()
     {
         $.ajax({
             url: '{{ url("cliente/alldatadirectivosgroup")}}/'
             , data: { evento: modelo.id_evento}
-            , method: 'GET'                             
+            , method: 'GET'
             , success: function(result){
-                
+
                 //var datoz = JSON.parse(result);
                 var datoz = result;
                 var html = '';
                 var elavatar = ''
                 var tempcss ='';
-                
+
                 for (var i = 0; i < datoz.length; i++)
                 {
                     tempcss ='';
@@ -525,33 +525,33 @@
                     else{
                         elavatar = "../../../adjuntos/"+datoz[i]['foto'];
                         tempcss ='';
-                    }	                                   
+                    }
                     @endif
                     //console.log({{$tipoevent}});
                     html +='<tr><th scope="row" style="vertical-align: text-bottom;"><img style="width:100px;heigth:100px;background:#fff !important;" src="'+ elavatar +'" alt="User Avatar"></th><td  style="vertical-align: text-bottom;font-weight:bold;">#' + datoz[i]['num_cliente'] +'</td><td  style="vertical-align: text-bottom;font-weight:bold;">'+ (datoz[i]["trato"].substring(0, 10)).toUpperCase()  +' '+ (datoz[i]["nombre"].substring(0, 10)).toUpperCase() +' '+ (datoz[i]["apellido"].substring(0, 10)).toUpperCase()  +' - '+ (datoz[i]["ocupacion"].substring(0, 10)).toUpperCase() +' '+ (datoz[i]["profesion"].substring(0, 10)).toUpperCase() +'</td></tr><tr><td colspan="3" style="vertical-align: text-bottom;">'+ (datoz[i]["memoria"]) +'</td></tr>';
                 }
-                $('#DirectivosDir').html(html);    
+                $('#DirectivosDir').html(html);
             }
         });
-    }      
-    
+    }
+
     $( document ).ready(function() {
         cargaadjuntosx(1);
         @if($tipoevent==2)
         evaluarasignararea();
         @endif
-    });  
-    
+    });
+
     function descargaradjunto(id)
     {
-        
+
         window.open( '{{ url('/') }}' + '/cliente/descargarfile/'+id );
     }
-    
-    
-    
-    
-    
+
+
+
+
+
 </script>
 
 
@@ -566,44 +566,44 @@
 <script>
     /*
     var socket = io("{{ env('PUBLISHER_URL') }}:{{ env('BROADCAST_PORT') }}");
-    
-    
-    socket.on('connect', function() 
+
+
+    socket.on('connect', function()
     {
         var parametros = ({ 'num_cliente' : modelo.num_cliente,'nombre' : modelo.nombre,'agencia' : modelo.agencia,'sala' : {{ $id_evento }} ,'name_evento' : encodeURI('{{ $nombreevento }}')   });
-        
+
         socket.emit('join', parametros, function(err) {
             if (err){
                 alert(err);
             }else{
-                
+
             }
         });
-        
+
         socket.on('bloquearexplorer',() => {
-            
+
             alert('AQUI LES DOY ACCION A TODOS');
-            
-        });        
-        
+
+        });
+
         socket.on('serviciomensajedirecto',(parametros) => {
-            
+
             alert('AQUI LES BLOQUEO' + parametros,mensaje);
-            
-        });   
-        
+
+        });
+
         socket.on('serviciomensajesala',(parametros) => {
-            
+
             alert('AQUI LES BLOQUEO' + parametros,mensaje);
-            
-        });    
-        
-        
+
+        });
+
+
         socket.on('disconnect', function() {
             console.log('User Disconnected to server');
         });
-        
-        socket.on('updateUserList', function(users) 
+
+        socket.on('updateUserList', function(users)
         {
             //console.log(users);
             var ol = '';
@@ -614,17 +614,17 @@
             });
             $('#usuarioslinea').html(ol);
         });
-        
+
         socket.on('serviciomensajedirecto', function(parametros) {
             alert('serviciomensajedirecto: '+parametros.mensaje);
         });
-        
+
         socket.on('serviciomensajesala', function(parametros) {
             alert('serviciomensajesala: '+parametros.mensaje);
         });
-        
-    });   
-    */ 
+
+    });
+    */
 </script>
     <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js"></script>
@@ -646,10 +646,10 @@
                     // user_name: document.getElementById('nombre_asoc').value,
                 },
             },
-            
+
             created() {
                 this.cargarPropuestas();
-            }, 
+            },
 
             methods: {
                 cargarPropuestas: function() {
@@ -665,24 +665,24 @@
                 registrar: function(event) {
                     event.preventDefault();
 
-                    if ( this.propuesta.titulo == null || this.propuesta.titulo == '' || this.propuesta.titulo ==undefined || this.propuesta.titulo.length <=0) 
+                    if ( this.propuesta.titulo == null || this.propuesta.titulo == '' || this.propuesta.titulo ==undefined || this.propuesta.titulo.length <=0)
                     {
                         lobibox_emergente('warning','top right',true,"Debe al menos, ingresar un titulo para su propuesta.");
-                        
+
                         return false;
                     }
 
-                    if ( this.propuesta.secunda_asoc == null || this.propuesta.secunda_asoc == '' || this.propuesta.secunda_asoc ==undefined || this.propuesta.secunda_asoc.length <=0) 
+                    if ( this.propuesta.secunda_asoc == null || this.propuesta.secunda_asoc == '' || this.propuesta.secunda_asoc ==undefined || this.propuesta.secunda_asoc.length <=0)
                     {
                         lobibox_emergente('warning','top right',true,"Debe ingresar el nombre de quien secunda su propuesta.");
-                        
+
                         return false;
                     }
 
-                    if ( this.propuesta.secunda_asoc_id == null || this.propuesta.secunda_asoc_id == '' || this.propuesta.secunda_asoc_id ==undefined || this.propuesta.secunda_asoc_id.length <=0) 
+                    if ( this.propuesta.secunda_asoc_id == null || this.propuesta.secunda_asoc_id == '' || this.propuesta.secunda_asoc_id ==undefined || this.propuesta.secunda_asoc_id.length <=0)
                     {
                         lobibox_emergente('warning','top right',true,"Debe ingresar un el número de asociado que secunda su propuesta.");
-                        
+
                         return false;
                     }
 
@@ -728,8 +728,4 @@
             }
         });
     </script>
-@stop	   
-
-
-
-
+@stop
