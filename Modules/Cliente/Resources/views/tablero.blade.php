e447497beadc2486ed65b44e00f0311ed1a3979d
Arturo Atencio <arturo.a.atencio@gmail.com>
2021-03-27 00:35:19 +0000

    agregando el boton de eliminar las propuestas de cada usuario y quitando el elipse de la tabla de admin

--- Modules/Cliente/Resources/views/tablero.blade.php
+++ Modules/Cliente/Resources/views/tablero.blade.php
@@ -130,13 +130,21 @@
                     </div>
                     <br><hr>
                     <div class="col-md-12">
-                        <h3>Listado de mis Propuestas</h3>
+                        <h3>Listado de Propuestas</h3>
                         <br>
                         <template>
                         <ul class="list-group">
-                            <li class="list-group-item" v-for="(item, index) in propuestas" :key="index">
+                            <li class="list-group-item" v-for="(item, index) in propuestas" :key="index"
+                                v-if="item.estado">
                                 <div class="d-flex justify-content-between align-items-center px-3" style="background-color: honeydew">
                                     <h5><strong class="mb-1" >@{{ item.titulo }}</strong></h5>
+                                    <button class="btn delete" v-if="item.user_id == {!! $num_cliente !!}" 
+                                        @click="aprobarPropuesta(item, false)" >
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
+                                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
+                                        </svg>
+                                    </button>
                                     {{-- <div>
                                         <button class="btn" @click="like(item.id)">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-thumbs-up" viewBox="0 0 16 16">
@@ -257,6 +265,23 @@
     .img-fluid {
         max-width: 56%;
         
+    }
+
+    .img-fluid {
+        max-width: 56%;
+        
+    }
+
+    button.delete>svg {
+        width: 20px;
+        height: 20px;
+        transition: width 0.5s ease;
+        transition: height 0.5s ease; 
+    }
+    button.delete>svg:hover {
+        color: red;
+        width: 35px;
+        height: 35px; 
     }
     
 </style>
@@ -696,6 +721,17 @@
                         }
                     }).catch( err => {
                         lobibox_emergente('warning','top right',true,"Hubo un error: " + err);
+                    });
+                },
+                aprobarPropuesta: function (item, estado) {
+                    axios.put('/cliente/propuesta/'+item.id, {
+                        estado: estado,
+                    })
+                    .then( response => {
+                        this.cargarPropuestas();
+                    })
+                    .catch( err => {
+                        alert("Hubo un error: " + err);
                     });
                 },
                 /* like: function(id) {
